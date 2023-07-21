ee1e5bda2a5948399658ad97952aed06f19b08d9
Johnrey Bacal <johnrey.bacal@yahoo.com>
2023-07-21 21:15:23 +0800

    Deploy build

--- src/service/activityService.ts
+++
@@ -1,9 +0,0 @@
-import type Activity from "@/types/activity";
-
-export default interface ActivityService {
-  list(): Array<Activity>;
-  get(id: string): Activity;
-  add(activity: Activity): void;
-  update(activity: Activity): void;
-  delete(id: string): void;
-}
--- src/types/activity.ts
+++
@@ -1,9 +0,0 @@
-import Quality from "./quality";
-
-export default class Activity {
-    id?: string;
-    name: string = '';
-    description?: string = '';
-    isDone: boolean = false;
-    qualities: Quality = new Quality();
-}
